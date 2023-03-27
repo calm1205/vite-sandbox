738c41cbcb8d5304993b93889bcbdbd34e9a0ffa
calm1205 <naginani072@gmail.com>
2023-03-27 20:05:26 +0900

    styled-reset

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 import { BrowserRouter } from "react-router-dom"
 import Router from "./Router"
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+`
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <Router />
     </BrowserRouter>
   </React.StrictMode>
