c491c2d5685f8152aa161d6c6c94ad5e30827bfa
calm1205 <naginani072@gmail.com>
2023-04-30 19:27:16 +0900

    [feature] update page title design

--- src/components/templates/DefaultLayout.tsx
+++ src/components/templates/DefaultLayout.tsx
@@ -1,22 +1,15 @@
-import { Typography } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 import { FC } from "react"
 import { Outlet } from "react-router-dom"
 
 export const DefaultLayout: FC = () => (
   <>
-    <header className="bg-sky-700">
-      <Typography
-        className="text-xl text-white h-14 flex items-center px-5 tracking-wide font-bold"
-        component="h1"
-      >
+    <Box component="header" bgcolor="#81d4fa">
+      <Typography component="h1" fontSize="30px" padding="10px">
         zustand
       </Typography>
-    </header>
+    </Box>
 
     <Outlet />
-
-    <footer className="bg-sky-900 h-10 w-full fixed bottom-0 text-center text-white items-center flex justify-center">
-      footer
-    </footer>
   </>
 )
